af8974a16b0028f66fbd6b6a73fc0a3c72340acd
Talit <talitasantos.hrzon@gmail.com>
2023-12-06 17:40:37 -0300

    chore(main): Tornar o TaskInherited o widget pai

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_estudos1/data/task_inherited.dart';
 import 'package:widgets_estudos1/screens/initial_screen.dart';
 
 void main() {
@@ -12,20 +13,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-          primaryColor: Colors.blue,
-          appBarTheme: const AppBarTheme(
-              color: Colors.blue,
-              titleTextStyle: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-              )),
-          useMaterial3: false,
-        ),
-        home: const InitialScreen(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        primaryColor: Colors.blue,
+        appBarTheme: const AppBarTheme(
+            color: Colors.blue,
+            titleTextStyle: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            )),
+        useMaterial3: false,
+      ),
+      home: TaskInherited(
+        child: const InitialScreen(),
+      ),
     );
   }
 }
-
