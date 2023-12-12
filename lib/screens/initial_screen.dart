c8c74d59b1e290452d26b781fbb47fe5d643d226
Talita <talitasantosrd@gmail.com>
2023-12-11 22:58:53 -0300

    Adicionar itens para status global

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -13,9 +13,60 @@ class _InitialScreenState extends State<InitialScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Container(),
-        title: const Text('Tarefas'),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(90.0),
+        child: SafeArea(
+          child: Container(
+              height: 100,
+              color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Tarefas',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: 240,
+                            child: LinearProgressIndicator(
+                              color: Colors.blue.shade100,
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 8.0),
+                            child: Text(
+                              'Level: 0.00',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 8.0),
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.autorenew_rounded,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )),
+        ),
       ),
       body: Container(
         color: Colors.blue.shade50,
@@ -28,7 +79,10 @@ class _InitialScreenState extends State<InitialScreen> {
         onPressed: () {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (contexNew) => FormScreen(taskContext: context,)),
+            MaterialPageRoute(
+                builder: (contexNew) => FormScreen(
+                      taskContext: context,
+                    )),
           );
         },
         child: const Icon(Icons.add),
