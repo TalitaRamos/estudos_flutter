3d59dd78485b1338bcfbdbdcbea2e2cafe5f5b1f
Talit <talitasantos.hrzon@gmail.com>
2023-12-13 19:52:30 -0300

    chore(form_screen): Iniciar nível com zero ao cadastrar task

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -145,6 +145,7 @@ class _FormScreenState extends State<FormScreen> {
                           nameController.text,
                           imageController.text,
                           int.parse(difficultyController.text),
+                          0
                         ));
 
                         ScaffoldMessenger.of(context).showSnackBar(
