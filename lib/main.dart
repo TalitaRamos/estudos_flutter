5f9c5cd4ec72ebdcc20e20f22006b70b765b7b7b
Talit <talitasantos.hrzon@gmail.com>
2023-12-13 19:52:41 -0300

    chore(main): Remover TaskInherited

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,7 @@ class MyApp extends StatelessWidget {
             )),
         useMaterial3: false,
       ),
-      home: TaskInherited(
-        child: const InitialScreen(),
-      ),
+      home: const InitialScreen(),
     );
   }
 }
